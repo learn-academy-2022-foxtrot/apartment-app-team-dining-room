1d280d0da45b77e90d5405f6bcc717c913ae5c49
c.r.macias56@hotmail.com <c.r.macias56@hotmail.com>
2022-11-03 16:04:36 -0700

    maybe fixed

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Apartment, type: :model do
   let(:user) { User.create email: "test@example.com", password: "password", password_confirmation: "password" }
 
   it "should have a valid street" do
-    apartment = user.apartments.create(
+    apartment = user.apartments.new(
       city: "Bikini Bottom",
       state: "Pacific Ocean",
       manager: "Mustachio Jones",
@@ -15,6 +15,7 @@ RSpec.describe Apartment, type: :model do
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
     )
+    apartment.validate
     expect(apartment.errors[:street]).to include "can't be blank"
   end
 end
