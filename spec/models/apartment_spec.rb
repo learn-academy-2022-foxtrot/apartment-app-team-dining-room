4b0bba088af04aeb290e768fe8ae8efc726743a9
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-11-04 10:16:18 -0700

    validations complete

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -18,4 +18,54 @@ RSpec.describe Apartment, type: :model do
     apartment.validate
     expect(apartment.errors[:street]).to include "can't be blank"
   end
+
+  it "is not valid without city" do
+    test = Apartment.create street: "Peidmont Avenue", state: "GA", manager: "Scully", email: "scully.person@gmail.com", price: "$2,300", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:city]).to_not be_empty
+  end
+
+  it "is not valid without state" do
+    test = Apartment.create street: "Peidmont Avenue", city: "bikini bottom", manager: "Scully", email: "scully.person@gmail.com", price: "$2,300", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:state]).to_not be_empty
+  end
+
+  it "is not valid without manager" do
+    test = Apartment.create street: "Peidmont Avenue", city: "bikini bottom", state: "GA", email: "scully.person@gmail.com", price: "$2,300", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:manager]).to_not be_empty
+  end
+
+  it "is not valid without email" do
+    test = Apartment.create street: "Peidmont Avenue", city: "bikini bottom", state: "GA", manager: "Mr krabs", price: "$2,300", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:email]).to_not be_empty
+  end
+
+  it "is not valid without price" do
+    test = Apartment.create street: "Peidmont Avenue", city: "bikini bottom", state: "GA", manager: "Mr krabs", email: "scully.person@gmail.com", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:price]).to_not be_empty
+  end
+
+  it "is not valid without bedrooms" do
+    test = Apartment.create street: "Peidmont Avenue", city: "bikini bottom", state: "GA", manager: "Mr krabs", email: "scully.person@gmail.com", price: "$1", bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:bedrooms]).to_not be_empty
+  end
+
+  it "is not valid without bathrooms" do
+    test = Apartment.create street: "Peidmont Avenue", city: "bikini bottom", state: "GA", manager: "Mr krabs", email: "scully.person@gmail.com", price: "$1", bedrooms: 3, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:bathrooms]).to_not be_empty
+  end
+
+  it "is not valid without pets" do
+    test = Apartment.create city: "Atlanta", state: "GA", manager: "Scully", email: "scully.person@gmail.com", price: "$2,300", bedrooms: 3, bathrooms: 2, street: "Peidmont Avenue", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", user_id: 1
+    expect(test.errors[:pets]).to_not be_empty
+  end
+
+  it "is not valid without image" do
+    test = Apartment.create city: "Atlanta", state: "GA", manager: "Scully", email: "scully.person@gmail.com", price: "$2,300", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", street: "Peidmont Avenue", user_id: 1
+    expect(test.errors[:image]).to_not be_empty
+  end
+
+  it "is not valid without user_id" do
+    test = Apartment.create city: "Atlanta", state: "GA", manager: "Scully", email: "scully.person@gmail.com", price: "$2,300", bedrooms: 3, bathrooms: 2, pets: "Cats and Dogs", image: "https://images1.apartments.com/i2/T-bZau3RhTVapgl8eyXgibxs9PAyYBm9A9E0kT42NUg/113/1824-defoor-atlanta-ga-primary-photo.jpg?p=1", street: "Peidmont Avenue"
+    expect(test.errors[:user_id]).to_not be_empty
+  end
 end
