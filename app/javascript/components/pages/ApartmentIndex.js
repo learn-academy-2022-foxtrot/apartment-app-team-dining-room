9a743354c891612d9f0a8c826c19fb8739dec0e4
Jeremy Duncan <jduncan46@my.devry.edu>
2022-11-04 14:19:21 -0400

    finished unprotected index

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,10 +1,28 @@
 import React from "react"
 
-const ApartmentIndex = () => {
+const ApartmentIndex = ({ apartments }) => {
   return (
-    <>
+    <div>
       <h3>ApartmentIndex</h3>
-    </>
+      {apartments.map((apartment, index) => {
+        return (
+            <div key={index}>
+            {apartment.street},
+            {apartment.city},
+            {apartment.state},
+            {/* {apartment.manager}, */}
+            {/* {apartment.email}, */}
+            {apartment.price},
+            {apartment.bedroom},
+            {apartment.bathrooms},
+            {/* {apartment.pets}, */}
+            {apartment.image},
+            {/* {apartment.user_id} */}
+            </div>
+        )
+      })}
+
+    </div>
   )
 }
 
--- app/javascript/components/pages/ApartmentIndex.test.js
+++ app/javascript/components/pages/ApartmentIndex.test.js
@@ -1,10 +1,11 @@
-import React from "react"
 import { render } from "@testing-library/react"
+import React from "react"
+import apartments from "../mockApartments"
 import ApartmentIndex from "./ApartmentIndex"
 
 describe("<ApartmentIndex />", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div")
-    render(<ApartmentIndex />, div)
+    render(<ApartmentIndex apartments={apartments} />, div)
   })
 })
